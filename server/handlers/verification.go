d0b7d78208da33add4df902e32794563b6bf9fb8
Mundane Lizard <mundanelizard@gmail.com>
2022-06-15 12:34:33 +0100

    basic scaffold for verification

--- server/handlers/verification.go
+++ server/handlers/verification.go
@@ -3,10 +3,43 @@ package handlers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/mundanelizard/koyi/server/handlers/middlewares"
+	"log"
+	"net/http"
 )
 
-func verifyPhoneNumberHandler(c *gin.Context) {
+func verifyHandler(c *gin.Context) {
+	var intentId string
+	var code string
+	var err error
 
+	if c.Request.Method == http.MethodPost {
+		var body map[string]string
+		err = c.BindJSON(body)
+	} else {
+		intentId = c.Param("intentId")
+		code = c.Param("code")
+	}
+
+	if err != nil {
+		log.Println(err)
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{})
+	}
+
+	ok, err := verifyUser(intentId, code)
+
+	if !ok {
+		log.Println(err)
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+
+	if err != nil {
+		log.Println(err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{})
+		return
+	}
+
+	c.AbortWithStatusJSON(http.StatusOK, gin.H{})
 }
 
 func CreateVerificationRoutes(router *gin.RouterGroup) {
